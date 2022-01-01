6df13c33a9707669e57674530b61871d02aad19f
Huyen TrAN <huyen.tran@code.berlin>
2022-01-01 22:54:43 +0100

    removed bcrypt

--- backend/data.js
+++ backend/data.js
@@ -1,20 +1,5 @@
 
-import bcrypt from 'bcryptjs';
 const data = {
-  users: [
-    {
-      name: 'Jin',
-      email: 'jin@g.com',
-      password: bcrypt.hashSync('jin', 8),
-      isAdmin: true,  
-    },
-    {
-      name: 'Jan',
-      email: 'jan@g.com',
-      password: bcrypt.hashSync('jan', 8),
-      isAdmin: false,  
-    }
-  ],
   products: [
     {
       name: "Yema Caramel Cake",
