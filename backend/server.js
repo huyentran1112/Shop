3f9dcd0247e6a1bf88e99fcdb4b8b791b2f5954f
Huyen TrAN <huyen.tran@code.berlin>
2022-01-02 11:34:30 +0100

    changed port

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,25 @@
 import express from 'express';
-import Mongoose  from 'mongoose';
-import userRouter from './routers/userRouter.js';
+import mongoose from 'mongoose';
 import productRouter from './routers/productRouter.js';
-
+import userRouter from './routers/userRouter.js';
 
 const app = express();
-Mongoose.connect('mongodb+srv://huyentran1112:GITisgood123$@cluster0.vxbij.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-  useNewURLParser: true,
+mongoose.connect( 'mongodb+srv://huyentran1112:GITisgood123$@cluster0.vxbij.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-const port = process.env.PORT || 5000;
-
-app.use(express.static(path.join(__dirname, '/frontend/build')));
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-);
-// app.get('/', (req, res) => {
-//   res.send('Server is ready');
-// });
-app.use('/api/myproducts', productRouter);
 app.use('/api/users', userRouter);
-app.use((err, req, res, next)=>{
-  res.status(500).send({message: err.message});
-})
+app.use('/api/products', productRouter);
+app.get('/', (req, res) => {
+  res.send('Server is ready');
+});
 
-app.listen(port, ()=> {
-  console.log('Test')
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
 });
